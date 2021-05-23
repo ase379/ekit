525175b39f91aea50114ad6235fd394d397c6eb7
ase379 <seehusen@medienpunkt.com>
2021-05-23 19:49:28 +0200

    #5
    + install escape listener

--- src/main/java/com/hexidec/ekit/dialogs/DialogFactory.java
+++ src/main/java/com/hexidec/ekit/dialogs/DialogFactory.java
@@ -5,6 +5,8 @@ import com.hexidec.ekit.EkitCore;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 /**
  * a factory for dialogs.
@@ -23,7 +25,9 @@ public class DialogFactory {
             instance = new DialogFactory();
         }
         return instance;
-    };
+    }
+
+    ;
 
     protected Point getTopLeftWindowPointForCurrentScreen(Window dialogOrFrame) {
         Dimension windowSize = dialogOrFrame.getSize();
@@ -31,73 +35,89 @@ public class DialogFactory {
         var gcBounds = gc.getBounds();
         var dx = gcBounds.x + (gcBounds.width - windowSize.width) / 2;
         var dy = gcBounds.y + (gcBounds.height - windowSize.height) / 2;
-        return new Point(dx,dy);
+        return new Point(dx, dy);
+    }
+
+
+    private void addEscapeListener(final JDialog dialog) {
+        ActionListener escListener = e -> dialog.setVisible(false);
+
+        dialog.getRootPane().registerKeyboardAction(escListener,
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
+
     }
 
     public void centerWindow(final Window window) {
         Rectangle dim = window.getGraphicsConfiguration().getBounds();
         var point = getTopLeftWindowPointForCurrentScreen(window);
-        window.setLocation(point.x , point.y);
+        window.setLocation(point.x, point.y);
     }
 
 
     public PropertiesDialog newPropertiesDialog(Frame parent, String[] fields, String[] types, String[] values, String title, boolean bModal) {
         PropertiesDialog dlg = new PropertiesDialog(parent, fields, types, values, title, bModal);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
     public JSpellDialog newSpellDialog(Frame owner, String title, boolean modal) {
-        JSpellDialog dlg = new JSpellDialog(owner,title, modal);
+        JSpellDialog dlg = new JSpellDialog(owner, title, modal);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
 
     public FontSelectorDialog newFontSelectorDialog(Frame parent, String title, boolean bModal, String attribName, String demoText) {
-        FontSelectorDialog dlg = new FontSelectorDialog(parent, title, bModal,attribName,demoText);
+        FontSelectorDialog dlg = new FontSelectorDialog(parent, title, bModal, attribName, demoText);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
 
     public ImageFileDialog newImageFileDialog(Frame parent, String imgDir, String[] imgExts, String imgDesc, String imgSrc, String title, boolean bModal) {
-        ImageFileDialog dlg = new ImageFileDialog(parent,imgDir, imgExts,imgDesc,imgSrc,title,bModal);
+        ImageFileDialog dlg = new ImageFileDialog(parent, imgDir, imgExts, imgDesc, imgSrc, title, bModal);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
 
     public SearchDialog newSearchDialog(Frame parent, String title, boolean bModal, boolean bIsReplace, boolean bCaseSetting, boolean bTopSetting) {
-        SearchDialog dlg = new SearchDialog(parent,title,bModal,bIsReplace,bCaseSetting,bTopSetting);
+        SearchDialog dlg = new SearchDialog(parent, title, bModal, bIsReplace, bCaseSetting, bTopSetting);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
     public ImageURLDialog newImageURLDialog(Frame parent, String title, boolean bModal) {
-        ImageURLDialog dlg = new ImageURLDialog(parent,title,bModal);
+        ImageURLDialog dlg = new ImageURLDialog(parent, title, bModal);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
     public SimpleInfoDialog newSimpleInfoDialog(Frame parent, String title, boolean bModal, String message, int type) {
-        SimpleInfoDialog dlg = new SimpleInfoDialog(parent,title,bModal,message,type);
+        SimpleInfoDialog dlg = new SimpleInfoDialog(parent, title, bModal, message, type);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
     public UserInputAnchorDialog newSimpleInfoDialog(EkitCore peKit, String title, boolean bModal, String defaultAnchor) {
-        UserInputAnchorDialog dlg = new UserInputAnchorDialog(peKit,title,bModal,defaultAnchor);
+        UserInputAnchorDialog dlg = new UserInputAnchorDialog(peKit, title, bModal, defaultAnchor);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
 
     public UnicodeDialog newUnicodeDialog(EkitCore parent, String title, boolean bModal, int indexr) {
-        UnicodeDialog dlg = new UnicodeDialog(parent,title,bModal,indexr);
+        UnicodeDialog dlg = new UnicodeDialog(parent, title, bModal, indexr);
+        addEscapeListener(dlg);
         centerWindow(dlg);
         return dlg;
     }
-
-
-
 }

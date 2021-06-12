dd2e0dec0fdd44969eb24a90d5a2ea068b52fd84
ase379 <seehusen@medienpunkt.com>
2021-06-12 21:12:03 +0200

    #18
    + need to set the paragraph attributes in a right way; base class did
      not replace them.

--- src/main/java/com/hexidec/ekit/action/AlignAction.java
+++ src/main/java/com/hexidec/ekit/action/AlignAction.java
@@ -22,25 +22,53 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 package com.hexidec.ekit.action;
 
 import java.awt.event.ActionEvent;
+import java.net.PasswordAuthentication;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
+import javax.swing.text.html.HTML;
 
 import com.hexidec.ekit.EkitCore;
 
 public class AlignAction extends StyledEditorKit.AlignmentAction
 {
 	protected EkitCore parent;
-
+	private final String alignment;
 	public AlignAction(EkitCore parent, String actionName, int actionType)
 	{
 		super(actionName, actionType);
+		switch (actionType) {
+			case StyleConstants.ALIGN_LEFT:
+				alignment = "left";
+				break;
+			case StyleConstants.ALIGN_RIGHT:
+				alignment = "right";
+				break;
+			case StyleConstants.ALIGN_CENTER:
+				alignment = "center";
+				break;
+			case StyleConstants.ALIGN_JUSTIFIED:
+				alignment = "justify";
+				break;
+			default:
+				throw new RuntimeException("Invalid StyleConstant ("+actionType+") for align");
+		}
 		this.parent = parent;
 	}
 
 	public void actionPerformed(ActionEvent ae)
 	{
+		// remember selection as it is lost when setting the attributes
 		int selStart = parent.getTextPane().getSelectionStart();
 		int selEnd = parent.getTextPane().getSelectionEnd();
-		super.actionPerformed(ae);
+
+		// get current attribute for the paragraph and remove and readd the align attribute
+		SimpleAttributeSet sasPara = new SimpleAttributeSet(parent.getTextPane().getParagraphAttributes());
+		sasPara.removeAttribute(HTML.Attribute.ALIGN);
+		sasPara.addAttribute(HTML.Attribute.ALIGN, alignment);
+		setParagraphAttributes(parent.getTextPane(),sasPara, true);
+
+		// get back the old state.
 		parent.getTextPane().setText(parent.getTextPane().getText());
 		parent.getTextPane().setSelectionStart(selStart);
 		parent.getTextPane().setSelectionEnd(selEnd);
